5e51dc1f5e1cad3c747f8b627e64c74106fe8bf7
Hudd <hededrk@gmail.com>
2016-02-01 12:06:01 +0300

    Daedalus: Add more test scripts

---
+++ scripts/Test/variables.d
@@ -0,0 +1,8 @@
+var int   v1;
+var float v2;
+const int c1 = 10;
+var int   v3[10];
+var int   v4[c1];
+const int c2[12] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+const int c3[c1] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+const int c4[c2[2]] = {1, 2};
