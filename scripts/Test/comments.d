c2c0731c82357c0639db6827e26f76e823aea808
Hudd <hededrk@gmail.com>
2016-02-10 13:03:01 +0300

    Daedalus: Add test script for comments

---
+++ scripts/Test/comments.d
@@ -0,0 +1,18 @@
+/* adasdasd */
+func void f() {}
+//sdadsads
+func void vv() {}
+/**/
+func void t(var /*/flo/at */ int x, var string /*whaaa/**** t*/ s) {}
+
+/*
+func void func1()
+{
+}
+//*/
+//*
+func void func2()
+{
+}
+//*/
+//
