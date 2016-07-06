774a4d1e3f65f8f4f5a65e14242de4e6a0252fd8
Hudd <hededrk@gmail.com>
2016-07-06 14:42:31 +0300

    symbols: Add Type struct
    I consider types to be of two parts: type itself (built-in type or
    user-defined* class) and type modifiers (const, array).
    
    * Gothic doesn't support user-defined types without some hacking,
    but it is a problem of "runtime", not language itself (which does
    support user-defined classes).

---
+++ include/daedalus/semantic/Type.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2016 hedede <haddayn@gmail.com>
+ *
+ * License LGPLv3 or later:
+ * GNU Lesser GPL version 3 <http://gnu.org/licenses/lgpl-3.0.html>
+ * This is free software: you are free to change and redistribute it.
+ * There is NO WARRANTY, to the extent permitted by law.
+ */
+#ifndef Daedalus_Type
+#define Daedalus_Type
+//#include <daedalus/semantic/Symbol.h>
+namespace daedalus {
+namespace tree {
+class Class;
+class Function;
+};
+
+struct TypeDef;
+struct Type {
+	TypeDef* baseType;
+	bool isConst;
+	unsigned count;
+};
+
+enum TypeID {
+	Integer,
+	Float,
+	String,
+	Function,
+	Class
+};
+
+struct TypeDef {
+	std::string name;
+	TypeID ty;
+};
+struct ClassDef : TypeDef {
+	tree::Class* ptr;
+};
+struct FuncDef : TypeDef {
+	tree::Function* ptr;
+};
+
+} // namespace daedalus
+#endif//Daedalus_Type
